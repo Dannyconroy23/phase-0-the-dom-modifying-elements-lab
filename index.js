afd6ca797ed569706be10e2d88e20389691e63cc
Daniel Conroy <dannyconroy23@hotmail.com>
2022-03-21 21:40:31 -0400

    commit tree 6d7a360222027793d1094b28c0b9d49da3da1e72

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here
+document.remove('main')
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+
+newHeader.textContent = 'Daniel is the champion'
+document.body.appen('newHeader')
